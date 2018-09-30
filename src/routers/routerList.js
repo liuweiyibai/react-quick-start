9db35a06888fa94a357cc6eaf3868ef118d16f2d
liuwei <lw1140@163.com>
2018-09-30 10:14:25 +0800

    update

---
+++ src/routers/routerList.js
@@ -0,0 +1,10 @@
+const RouterLists = [{
+  path: "/dashboard",
+  sidebarName: "Dashboard",
+  navbarName: "Material Dashboard",
+  icon: 'Dashboard',
+  component: 'DashboardPage',
+  Auth:true
+}];
+
+export default RouterLists;
